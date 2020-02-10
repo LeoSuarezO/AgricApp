cbe52063b326d51bedcf4a6a17de311a468f24c8
LEONARDO <LEONARDO@LAPTOP-AJA6J6MV>
2020-02-09 21:36:16 -0500

    Revert "export"
    This reverts commit 9d0807e8036d6e14c982165851e22b60c3501515.

--- agriculture_app/src/controller/AppManager.java
+++ agriculture_app/src/controller/AppManager.java
@@ -4,36 +4,38 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Locale;
+
 import org.json.simple.DeserializationException;
+
 import general.HandlerLanguage;
 import models.CropManager;
 import models.CropTransitory;
 import persistence.Manager;
-import view.JDialogAddCroop;
+import view.JDialogAddCrop;
 import view.PrinFrame;
 
 public class AppManager implements ActionListener{
-//hola
+
 	private static final String NAME_FILE_CONFIG = "config.init";
 	private HandlerLanguage config = null;
 	private String languageDefault;
 	private PrinFrame frame;
-	private JDialogAddCroop jDialog;
-	public CropManager cropManager; 
+	private JDialogAddCrop jDialog;
 	public Manager fileM;
 
 	public AppManager() throws DeserializationException, IOException {
 		loadConfiguration();
 		fileM = new Manager();
-		frame = new PrinFrame(this);		
+		frame = new PrinFrame(this);	
+		jDialog = new JDialogAddCrop(this);
 	}
 
 	public void addElementsToTable() {
 		for (int i = 0; i < CropManager.getListCropTr().size(); i++) {
 			CropTransitory crop = CropManager.getListCropTr().get(i);
 			frame.addElementToTable(crop.toObjectVector());
+			}
 		}
-	}
 
 	public void changePanelT() {
 		frame.changePanelT();
@@ -42,7 +44,7 @@ public class AppManager implements ActionListener{
 	public void changePanelAdm() {
 		frame.changePanelAdm();
 	}
-
+	
 	public String getLanguageDefault(){
 		languageDefault = Locale.getDefault().getLanguage();
 		switch (languageDefault) {
@@ -112,6 +114,18 @@ public class AppManager implements ActionListener{
 
 	private void manageChangeLanguage(){
 		frame.changeLanguage();
+		jDialog.changeLanguage();
+	}
+	
+	public void createCrop() {
+		jDialog.newCrop();
+		addElementsToTable();
+		jDialog.dispose();;
+	}
+	
+	public void exportNewCrops() throws IOException {
+		fileM.writeNewCrops();
+	    frame.succes();
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -130,17 +144,20 @@ public class AppManager implements ActionListener{
 			changePanelAdm();
 			break;
 		case HOME:
-			frame = new PrinFrame(this);
+			System.err.println("Opcion en proceso de implementacion");
 			break;
 		case CREATE_CROOP:
-			CropManager.createCropTr(jDialog.getTxtNameCroop(),
-					jDialog.getTxtCropArea(), jDialog.getTxtPlantingArea(), jDialog.getTxtTons(), jDialog.getJcTypeCroop(), jDialog.getTxtPrice(), jDialog.getTxtCosts());
-			addElementsToTable();
-			jDialog.setVisible(false);
+			createCrop();
 			break;
 		case ADD_CROOP:
-			jDialog = new JDialogAddCroop(this);
+			jDialog.setVisible(true);
 			break;
+		case EXPORT:
+			try {
+				exportNewCrops();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		default:
 			break;
 		}
