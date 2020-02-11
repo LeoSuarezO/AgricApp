7bd36603aa9764f617e77c3f12c01178b31774bd
LEONARDO <LEONARDO@LAPTOP-AJA6J6MV>
2020-02-10 22:41:36 -0500

    Panel home

---
+++ agriculture_app/src/view/PanelHome.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class PanelHome extends JPanel{
+	
+	public PanelHome() {
+		
+	}
+	
+	public void init() {
+		
+	}
+	
+	public void config() {
+		
+	}
+
+}
---
+++ agriculture_app/src/view/PanelHomeP.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelHomeP extends JPanel{
+	
+	private JLabel img;
+	private JLabel date;
+	private JLabel text;
+	private String path;
+	private String dateS;
+	private String descS;
+	private JLabel desc;
+	private JPanel panel;
+	
+	public PanelHomeP() {
+
+	}
+	
+	public void init() {
+		
+	}
+	
+	public void config() {
+		
+	}
+
+}
